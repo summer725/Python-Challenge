a6ad6fbe19dc0841cd681fc8067822dd0baf707c
Summer Breeze <SummerBaptiste2021@u.northwestern.edu>
2021-01-08 20:47:30 -0600

    Finished calculations for the differences with summary

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -12,16 +12,34 @@ print("------------------------")
 with open_csv as f:
     bankreader = csv.reader(f, delimiter = ",")
     header = next(bankreader)
-
-    # Counting the # of dates in the the dataset
-    #total = len(list(bankreader))
-    #print(total)
-
+    
     # Find the net profits/losses for the entire set
     sums = 0
     count = 0
+    previous = 0
+    diff = [] #setting up to be a list
+    months = [] #setting up to be a list
     for row in bankreader:
+        diff = diff + [(float(row[1])- previous)]
+        months = months + [row[0]]
         sums = sums + float(row[1])
         count += 1
+        #print(float(row[1])-previous)
+        previous = float(row[1])
+        
     print("Total Months: ", count)
-    print("Total: ", '$%s' %(sums))
+    print("Total: ", '$%s' %(round(sums)))
+    x = sum(diff[1:])/(count-1)
+    print(f"Average Change: ${x:.2f}")
+    #print(diff)
+    #Max Change
+    #print(diff.index(max(diff)))
+    #print(months)
+    print("Greatest Increase in Profits: ", months[diff.index(max(diff))], '($%s)' %(round(max(diff)))) # max month name 
+    #print(max(diff)) # max value
+
+    #Min Change
+    #print(diff.index(min(diff)))
+    #print(months)
+    print("Greatest Decrease in Profits: ", months[diff.index(min(diff))], '($%s)' %(round(min(diff)))) # min month name
+    #print(min(diff)) #min value
